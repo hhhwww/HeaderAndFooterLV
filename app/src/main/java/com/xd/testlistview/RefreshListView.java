a4c66badd123e9c3d512d8b9d578147dbabe93b3
He Wei <xdhhhwww@foxmail.com>
2016-03-17 17:06:29 +0800

    完成

--- app/src/main/java/com/xd/testlistview/RefreshListView.java
+++ app/src/main/java/com/xd/testlistview/RefreshListView.java
@@ -5,7 +5,10 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 /**
  * Created by hhhhwei on 16/3/17.
@@ -14,6 +17,17 @@ public class RefreshListView extends ListView {
 
     private View mHeaderView;
     private int mHeaderViewHeight;
+
+    private static final int STATE_PULL_REFRESH = 0;//下拉刷新
+    private static final int STATE_RELEASE_REFRESH = 1;//松开刷新
+    private static final int STATE_REFRESHING = 2;//正在刷新
+
+    private int mCurrentState = 0;
+
+    private TextView tvTitle;
+    private TextView tvDate;
+    private ImageView ivArrow;
+    private ProgressBar pbProgress;
 
     public RefreshListView(Context context) {
         this(context, null);
@@ -31,6 +45,11 @@ public class RefreshListView extends ListView {
     private void initHeaderView(Context context) {
         mHeaderView = LayoutInflater.from(context).inflate(R.layout.header, null);
         this.addHeaderView(mHeaderView);
+
+        tvTitle = (TextView) mHeaderView.findViewById(R.id.tv_title);
+        tvDate = (TextView) mHeaderView.findViewById(R.id.tv_date);
+        ivArrow = (ImageView) mHeaderView.findViewById(R.id.iv_arr);
+        pbProgress = (ProgressBar) mHeaderView.findViewById(R.id.pb_circle);
 
         //隐藏headerview
         mHeaderView.measure(0, 0);
@@ -59,15 +78,61 @@ public class RefreshListView extends ListView {
                 if (dy > 0 && getFirstVisiblePosition() == 0) {
                     int padding = dy - mHeaderViewHeight;
                     mHeaderView.setPadding(0, padding, 0, 0);
+
+                    //将状态该为松开刷新
+                    if (padding > 0 && mCurrentState != STATE_RELEASE_REFRESH) {
+                        mCurrentState = STATE_RELEASE_REFRESH;
+                        refreshState();
+                    }
+                    //将状态改为下拉刷新
+                    else if (padding < 0 && mCurrentState != STATE_PULL_REFRESH) {
+                        mCurrentState = STATE_PULL_REFRESH;
+                        refreshState();
+                    }
+
                     return true;
                 }
                 break;
 
+            //在这里处理各个状态后的布局位置操作,收尾
             case MotionEvent.ACTION_UP:
                 //重置操作
                 startY = -1;
+
+                if (mCurrentState == STATE_RELEASE_REFRESH) {
+                    mCurrentState = STATE_REFRESHING;
+                    mHeaderView.setPadding(0, 0, 0, 0);
+                    refreshState();
+                } else if (mCurrentState == STATE_PULL_REFRESH) {
+                    //因为刚开始就是下拉刷新，所以不需要初始化了,只需要把半截子的mHeaderView隐藏就好
+                    mHeaderView.setPadding(0, 0, -mHeaderViewHeight, 0);
+                }
+
                 break;
         }
         return super.onTouchEvent(ev);
+    }
+
+    //根据状态改变布局
+    private void refreshState() {
+        switch (mCurrentState) {
+            case STATE_PULL_REFRESH:
+                tvTitle.setText("下拉刷新");
+                ivArrow.setVisibility(VISIBLE);
+                pbProgress.setVisibility(INVISIBLE);
+                break;
+
+            case STATE_RELEASE_REFRESH:
+                tvTitle.setText("松开刷新");
+                ivArrow.setVisibility(VISIBLE);
+                pbProgress.setVisibility(INVISIBLE);
+                break;
+
+            case STATE_REFRESHING:
+                tvTitle.setText("正在刷新");
+                ivArrow.setVisibility(INVISIBLE);
+                pbProgress.setVisibility(VISIBLE);
+                break;
+        }
     }
 }
