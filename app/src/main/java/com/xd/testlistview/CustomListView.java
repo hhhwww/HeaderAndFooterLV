e76265ebc5619c4b623b5fc636b173aae61e6b9e
He Wei <xdhhhwww@foxmail.com>
2016-03-17 15:56:01 +0800

    删除不必要的代码

--- app/src/main/java/com/xd/testlistview/CustomListView.java
+++ app/src/main/java/com/xd/testlistview/CustomListView.java
@@ -3,7 +3,6 @@ package com.xd.testlistview;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.ListView;
@@ -11,14 +10,14 @@ import android.widget.ListView;
 /**
  * Created by hhhhwei on 16/3/17.
  */
-public class CustomListView extends ListView implements AbsListView.OnScrollListener, View.OnTouchListener {
+public class CustomListView extends ListView implements AbsListView.OnScrollListener {
 
     private View footer;
 
     private boolean isLoading;
-    private View headLayout;
-    private int headerViewHeight;
-    private View headerView;
+//    private View headLayout;
+//    private int headerViewHeight;
+//    private View headerView;
 
     public CustomListView(Context context) {
         this(context, null);
@@ -31,7 +30,7 @@ public class CustomListView extends ListView implements AbsListView.OnScrollList
     public CustomListView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         initFootView(context);
-        initHeaderView(context);
+//        initHeaderView(context);
     }
 
     public void initFootView(Context context) {
@@ -41,7 +40,6 @@ public class CustomListView extends ListView implements AbsListView.OnScrollList
         footer.findViewById(R.id.ll_foot).setVisibility(INVISIBLE);
 
         this.setOnScrollListener(this);
-        this.setOnTouchListener(this);
     }
 
     @Override
@@ -50,19 +48,18 @@ public class CustomListView extends ListView implements AbsListView.OnScrollList
             footer.findViewById(R.id.ll_foot).setVisibility(VISIBLE);
             loadingListener.load();
         }
-        this.scrollState = scrollState;
     }
 
-    //用于头布局
+    //用于尾布局
     private int lastVisibleItem = 0;
     private int totalCount = 0;
 
-    //用于尾布局
-    private int firstVisibleItem;
+    //用于头布局
+//    private int firstVisibleItem;
 
     @Override
     public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-        this.firstVisibleItem = firstVisibleItem;
+//        this.firstVisibleItem = firstVisibleItem;
         this.lastVisibleItem = firstVisibleItem + visibleItemCount;
         this.totalCount = totalItemCount;
     }
@@ -83,92 +80,92 @@ public class CustomListView extends ListView implements AbsListView.OnScrollList
     }
 
     //操作头布局
-    public void initHeaderView(Context context) {
-        LayoutInflater layoutInflater = LayoutInflater.from(context);
-        headLayout = layoutInflater.inflate(R.layout.header, null);
-        headerView = headLayout.findViewById(R.id.rl_header);
-        this.addHeaderView(headLayout);
-
-        init();
-    }
-
-    private void init() {
-        //让它强制调用measue,后面就可以使用getMeasuredHeight了。
-        headerView.measure(0, 0);
-        headerViewHeight = headerView.getMeasuredHeight();
-        headerView.setPadding(headerView.getPaddingLeft(), -headerViewHeight, headerView.getPaddingRight(),
-                headerView.getPaddingBottom());
-        headerView.postInvalidate();
-    }
-
-    private boolean isRemark;//标记当前是否是在listview的最顶端按下的
-    private int startY;//按下时候的Y值
-
-    //当按下时，并且根据firstVisibleItem发现它在最顶端，则需要添加标记并记录y值.
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                if (firstVisibleItem == 0)
-                    isRemark = true;
-                startY = (int) event.getY();
-                break;
-
-            case MotionEvent.ACTION_UP:
-                break;
-
-            case MotionEvent.ACTION_MOVE:
-                onMove(event);
-                break;
-        }
-        //可点击的都会返回true
-        return super.onTouchEvent(event);
-    }
-
-    private int mState;
-    //定义四个状态
-    private static final int STATE_NORMAL = 0;
-    private static final int STATE_PULL = 1;
-    private static final int STATE_RELEASE = 2;
-    private static final int STATE_REFRESHING = 3;
-
-    //记录当前的滚动状态
-    private int scrollState;
-
-    //判断一下移动过程中的操作
-    public void onMove(MotionEvent event) {
-        if (!isRemark)
-            return;
-
-        int endY = (int) event.getY();
-        int dy = endY - startY;
-
-        //一直需要改变的高度
-        int topPadding = dy - headerViewHeight;
-
-        switch (mState) {
-            case STATE_NORMAL:
-                if (dy > 0) mState = STATE_PULL;
-                break;
-
-            case STATE_PULL:
-                if (dy > headerViewHeight + 30 && scrollState == SCROLL_STATE_TOUCH_SCROLL)
-                    mState = STATE_RELEASE;
-                if (dy < 0) {
-                    mState = STATE_NORMAL;
-                    isRemark = false;
-                }
-                break;
-
-            case STATE_RELEASE:
-                if (dy < headerViewHeight + 30)
-                    mState = STATE_PULL;
-                break;
-
-            case STATE_REFRESHING:
-                break;
-        }
-        headerView.setPadding(headerView.getPaddingLeft(),
-                topPadding, headerView.getPaddingRight(), headerView.getPaddingBottom());
-    }
+//    public void initHeaderView(Context context) {
+//        LayoutInflater layoutInflater = LayoutInflater.from(context);
+//        headLayout = layoutInflater.inflate(R.layout.header, null);
+//        headerView = headLayout.findViewById(R.id.rl_header);
+//        this.addHeaderView(headLayout);
+//
+//        init();
+//    }
+//
+//    private void init() {
+//        //让它强制调用measue,后面就可以使用getMeasuredHeight了。
+//        headerView.measure(0, 0);
+//        headerViewHeight = headerView.getMeasuredHeight();
+//        headerView.setPadding(headerView.getPaddingLeft(), -headerViewHeight, headerView.getPaddingRight(),
+//                headerView.getPaddingBottom());
+//        headerView.postInvalidate();
+//    }
+//
+//    private boolean isRemark;//标记当前是否是在listview的最顶端按下的
+//    private int startY;//按下时候的Y值
+//
+//    //当按下时，并且根据firstVisibleItem发现它在最顶端，则需要添加标记并记录y值.
+//    @Override
+//    public boolean onTouch(View v, MotionEvent event) {
+//        switch (event.getAction()) {
+//            case MotionEvent.ACTION_DOWN:
+//                if (firstVisibleItem == 0)
+//                    isRemark = true;
+//                startY = (int) event.getY();
+//                break;
+//
+//            case MotionEvent.ACTION_UP:
+//                break;
+//
+//            case MotionEvent.ACTION_MOVE:
+//                onMove(event);
+//                break;
+//        }
+//        //可点击的都会返回true
+//        return super.onTouchEvent(event);
+//    }
+//
+//    private int mState;
+//    //定义四个状态
+//    private static final int STATE_NORMAL = 0;
+//    private static final int STATE_PULL = 1;
+//    private static final int STATE_RELEASE = 2;
+//    private static final int STATE_REFRESHING = 3;
+//
+//    //记录当前的滚动状态
+//    private int scrollState;
+//
+//    //判断一下移动过程中的操作
+//    public void onMove(MotionEvent event) {
+//        if (!isRemark)
+//            return;
+//
+//        int endY = (int) event.getY();
+//        int dy = endY - startY;
+//
+//        //一直需要改变的高度
+//        int topPadding = dy - headerViewHeight;
+//
+//        switch (mState) {
+//            case STATE_NORMAL:
+//                if (dy > 0) mState = STATE_PULL;
+//                break;
+//
+//            case STATE_PULL:
+//                if (dy > headerViewHeight + 30 && scrollState == SCROLL_STATE_TOUCH_SCROLL)
+//                    mState = STATE_RELEASE;
+//                if (dy < 0) {
+//                    mState = STATE_NORMAL;
+//                    isRemark = false;
+//                }
+//                break;
+//
+//            case STATE_RELEASE:
+//                if (dy < headerViewHeight + 30)
+//                    mState = STATE_PULL;
+//                break;
+//
+//            case STATE_REFRESHING:
+//                break;
+//        }
+//        headerView.setPadding(headerView.getPaddingLeft(),
+//                topPadding, headerView.getPaddingRight(), headerView.getPaddingBottom());
+//    }
 }
